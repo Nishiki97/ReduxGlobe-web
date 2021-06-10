cef4a06bf7fa9aa2c8e942c3259464fd6b466173
N A Yapa <it18002102@my.sliit.lk>
2021-06-10 08:44:57 +0530

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello ReduxGlobe</h1>
+      <Router>
+        <Switch>
+          <Header/>
+          <Route path="/aboutUs" exact component={AboutUs}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Navbar, Button, Nav } from 'react-bootstrap';
+import logo from '../images/globe.png';
+
+const Header = () => {
+    return(
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand>
+                <img
+                    alt=""
+                    src={logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                ReduxGlobe
+            </Navbar.Brand>
+            <Nav className="mr-auto"></Nav>
+            <Button variant="outline-info">About Us</Button>
+    
+        </Navbar>
+    );
+}
+
+export default Header;
